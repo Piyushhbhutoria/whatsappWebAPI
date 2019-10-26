5b14a5f7433f8072aa721e62e9dc5f0ab90e673a
Piyushhbhutoria <piyush.bhutoria98@gmail.com>
2019-10-27 00:59:12 +0530

    update

--- example/go.mod
+++ example/go.mod
@@ -5,7 +5,8 @@ go 1.12
 require (
 	github.com/Baozisoftware/qrcode-terminal-go v0.0.0-20170407111555-c0650d8dff0f
 	github.com/Rhymen/go-whatsapp v0.0.2
-	github.com/getsentry/sentry-go v0.2.1
+	github.com/getsentry/sentry-go v0.3.0
 	github.com/gin-contrib/cors v1.3.0
 	github.com/gin-gonic/gin v1.4.0
+	golang.org/x/sync v0.0.0-20181221193216-37e7f081c4d4 // indirect
 )
--- example/main.go
+++ example/main.go
@@ -4,12 +4,6 @@ import (
 	"encoding/gob"
 	"encoding/json"
 	"fmt"
-	qrcodeTerminal "github.com/Baozisoftware/qrcode-terminal-go"
-	"github.com/Rhymen/go-whatsapp"
-	"github.com/getsentry/sentry-go"
-	sentrygin "github.com/getsentry/sentry-go/gin"
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
 	"os"
@@ -17,6 +11,13 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	qrcodeTerminal "github.com/Baozisoftware/qrcode-terminal-go"
+	"github.com/Rhymen/go-whatsapp"
+	"github.com/getsentry/sentry-go"
+	sentrygin "github.com/getsentry/sentry-go/gin"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 type SendText struct {
@@ -76,7 +77,7 @@ func init() {
 		fmt.Printf("Sentry initialization failed: %v\n", err)
 	}
 
-	requestChannel = make(chan whatsapp.TextMessage,runtime.NumCPU())
+	requestChannel = make(chan whatsapp.TextMessage, runtime.NumCPU())
 
 	wac.AddHandler(&waHandler{wac})
 	if err = login(wac); err != nil {
@@ -157,127 +158,127 @@ func main() {
 
 func (*waHandler) HandleTextMessage(message whatsapp.TextMessage) {
 	//if message.Info.Timestamp > uint64(now) && !message.Info.FromMe && len(message.Text) < 17 && len(message.Text) > 1 {
-		if message.Info.Timestamp > uint64(now) && !message.Info.FromMe && !strings.Contains(message.Text, "@g.us") && len(message.Text) < 21 {
-			fmt.Printf("%v from %v\n", message.Text, message.Info)
-			requestChannel <- message
+	if message.Info.Timestamp > uint64(now) && !message.Info.FromMe && !strings.Contains(message.Text, "@g.us") && len(message.Text) < 21 {
+		fmt.Printf("%v from %v\n", message.Text, message.Info)
+		requestChannel <- message
+	}
+}
+
+func ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
+func helloworld(c *gin.Context) {
+	c.String(http.StatusOK, "Hello World")
+}
+
+func sendText(c *gin.Context) {
+	to := strings.Replace(c.DefaultQuery("to", "1234567890"), " ", "", -1)
+	mess := c.DefaultQuery("msg", "testing")
+	c.String(http.StatusOK, texting(to, mess))
+}
+
+func sendBulk(c *gin.Context) {
+	var data SendBulkText
+	m := make(map[string]string)
+
+	if err := c.ShouldBindJSON(&data); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	for _, each := range data.List {
+		each.Receiver = strings.Replace(each.Receiver, " ", "", -1)
+		if each.Receiver != "" {
+			m[each.Receiver] = texting(each.Receiver, each.Message)
 		}
 	}
+	c.JSON(http.StatusOK, data)
+}
 
-	func ping(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+func texting(to, mess string) string {
+	msg := whatsapp.TextMessage{
+		Info: whatsapp.MessageInfo{
+			RemoteJid: "91" + to + "@s.whatsapp.net",
+		},
+		Text: mess,
 	}
 
-	func helloworld(c *gin.Context) {
-		c.String(http.StatusOK, "Hello World")
+	msgId, err := wac.Send(msg)
+	if err != nil {
+		log.Printf("Error sending message: to %v --> %v\n", to, err)
+		return "Error"
 	}
+	return "Message Sent -> " + to + " : " + msgId
+}
 
-	func sendText(c *gin.Context) {
-		to := strings.Replace(c.DefaultQuery("to", "1234567890"), " ", "", -1)
-		mess := c.DefaultQuery("msg", "testing")
-		c.String(http.StatusOK, texting(to, mess))
-	}
-
-	func sendBulk(c *gin.Context) {
-		var data SendBulkText
-		m := make(map[string]string)
-
-		if err := c.ShouldBindJSON(&data); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		for _, each := range data.List {
-			each.Receiver = strings.Replace(each.Receiver, " ", "", -1)
-			if each.Receiver != "" {
-				m[each.Receiver] = texting(each.Receiver, each.Message)
-			}
-		}
-		c.JSON(http.StatusOK, data)
-	}
-
-	func texting(to, mess string) string {
-		msg := whatsapp.TextMessage{
-			Info: whatsapp.MessageInfo{
-				RemoteJid: "91" + to + "@s.whatsapp.net",
-			},
-			Text: mess,
-		}
-
-		msgId, err := wac.Send(msg)
+func login(wac *whatsapp.Conn) error {
+	//load saved session
+	session, err := readSession()
+	if err == nil {
+		//restore session
+		session, err = wac.RestoreWithSession(session)
 		if err != nil {
-			log.Printf("Error sending message: to %v --> %v\n", to, err)
-			return "Error"
+			return fmt.Errorf("restoring failed: %v\n", err)
 		}
-		return "Message Sent -> " + to + " : " + msgId
-	}
-
-	func login(wac *whatsapp.Conn) error {
-		//load saved session
-		session, err := readSession()
-		if err == nil {
-			//restore session
-			session, err = wac.RestoreWithSession(session)
-			if err != nil {
-				return fmt.Errorf("restoring failed: %v\n", err)
-			}
-		} else {
-			//no saved session -> regular login
-			qrChan := make(chan string)
-			go func() {
-				obj := qrcodeTerminal.New2(qrcodeTerminal.ConsoleColors.BrightBlue, qrcodeTerminal.ConsoleColors.BrightGreen, qrcodeTerminal.QRCodeRecoveryLevels.Low)
-				obj.Get(<-qrChan).Print()
-			}()
-			session, err = wac.Login(qrChan)
-			if err != nil {
-				return fmt.Errorf("error during login: %v\n", err)
-			}
-		}
-
-		//save session
-		if err = writeSession(session); err != nil {
-			return fmt.Errorf("error saving session: %v\n", err)
-		}
-		return nil
-	}
-
-	func readSession() (whatsapp.Session, error) {
-		session := whatsapp.Session{}
-		file, err := os.Open(os.TempDir() + "/whatsapp.gob")
+	} else {
+		//no saved session -> regular login
+		qrChan := make(chan string)
+		go func() {
+			obj := qrcodeTerminal.New2(qrcodeTerminal.ConsoleColors.BrightBlue, qrcodeTerminal.ConsoleColors.BrightGreen, qrcodeTerminal.QRCodeRecoveryLevels.Low)
+			obj.Get(<-qrChan).Print()
+		}()
+		session, err = wac.Login(qrChan)
 		if err != nil {
-			return session, err
+			return fmt.Errorf("error during login: %v\n", err)
 		}
-		defer file.Close()
-		decoder := gob.NewDecoder(file)
-		if err = decoder.Decode(&session); err != nil {
-			return session, err
-		}
-		return session, nil
 	}
 
-	func writeSession(session whatsapp.Session) error {
-		file, err := os.Create(os.TempDir() + "/whatsapp.gob")
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-		if err = gob.NewEncoder(file).Encode(session); err != nil {
-			return err
-		}
-		return nil
+	//save session
+	if err = writeSession(session); err != nil {
+		return fmt.Errorf("error saving session: %v\n", err)
 	}
+	return nil
+}
 
-	func (h *waHandler) HandleError(err error) {
-		if e, ok := err.(*whatsapp.ErrConnectionFailed); ok {
-			log.Printf("Connection failed, underlying error: %v/n", e.Err)
-			log.Println("Waiting 30sec...")
-			<-time.After(30 * time.Second)
-			log.Println("Reconnecting...")
-			if err := h.c.Restore(); err != nil {
-				log.Printf("Restore failed: %v", err)
-			}
-		} else {
-			log.Printf("error occoured: %v\n", err)
-		}
+func readSession() (whatsapp.Session, error) {
+	session := whatsapp.Session{}
+	file, err := os.Open(os.TempDir() + "/whatsapp.gob")
+	if err != nil {
+		return session, err
 	}
+	defer file.Close()
+	decoder := gob.NewDecoder(file)
+	if err = decoder.Decode(&session); err != nil {
+		return session, err
+	}
+	return session, nil
+}
+
+func writeSession(session whatsapp.Session) error {
+	file, err := os.Create(os.TempDir() + "/whatsapp.gob")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	if err = gob.NewEncoder(file).Encode(session); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (h *waHandler) HandleError(err error) {
+	if e, ok := err.(*whatsapp.ErrConnectionFailed); ok {
+		log.Printf("Connection failed, underlying error: %v/n", e.Err)
+		log.Println("Waiting 30sec...")
+		<-time.After(30 * time.Second)
+		log.Println("Reconnecting...")
+		if err := h.c.Restore(); err != nil {
+			log.Printf("Restore failed: %v", err)
+		}
+	} else {
+		log.Printf("error occoured: %v\n", err)
+	}
+}
