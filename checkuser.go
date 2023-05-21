fe2497cef82c1ed0012afcc6c552c7aec773f410
Piyushh Bhutoria <piyush.bhutoria98@gmail.com>
2023-05-21 19:28:01 +0530

    fixed lint

--- checkuser.go
+++ checkuser.go
@@ -6,14 +6,16 @@ func checkuser(args []string) bool {
 		log.Errorf("Failed to check if users are on WhatsApp:", err)
 		return false
 	}
-	for _, item := range resp {
-		if item.VerifiedName != nil {
-			log.Infof("%s: on whatsapp: %t, JID: %s, business name: %s", item.Query, item.IsIn, item.JID, item.VerifiedName.Details.GetVerifiedName())
-		} else {
-			log.Infof("%s: on whatsapp: %t, JID: %s", item.Query, item.IsIn, item.JID)
-		}
-		return item.IsIn
+	if len(resp) == 0 {
+		log.Infof("No results")
+		return false
 	}
 
-	return false
+	item := resp[0]
+	if item.VerifiedName != nil {
+		log.Infof("%s: on whatsapp: %t, JID: %s, business name: %s", item.Query, item.IsIn, item.JID, item.VerifiedName.Details.GetVerifiedName())
+	} else {
+		log.Infof("%s: on whatsapp: %t, JID: %s", item.Query, item.IsIn, item.JID)
+	}
+	return item.IsIn
 }
